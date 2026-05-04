1ed148df4501772d3ca60f6037cb715d36221196
CamDawg <pcamagna@yahoo.com>
2026-05-03 19:50:42 -0700

    v8 prep, one last EEFP warning

--- package_mod.bat
+++ package_mod.bat
@@ -10,7 +10,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Spell-50"
-set mod_version=v7
+set mod_version=v8
 set mod_folder=spell50
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
